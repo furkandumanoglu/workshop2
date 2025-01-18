d35d14b3ddc4316b3dab25d5602c68a398116a3f
Furkan Dumanoglu <dumanoglufurkan@hotmail.com>
2025-01-17 22:08:36 -0500

    Fixed settings and Docker Compose configuration

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
-    STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-    
+
 
 STATIC_URL = '/static/'
 
